60b7a1b780e0b02bcd4c06655aebbd6a1639ed3f
kongemom1215 <kongemom1215@gmail.com>
2021-02-17 14:42:37 +0900

    juhye commit1

---
+++ src/main/java/com/spirng/bom/BomApplication.java
@@ -0,0 +1,13 @@
+package com.spirng.bom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spirng/bom/BomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spirng.bom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
